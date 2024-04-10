7e90e7286c28f6184f39434313d7c00436011812
goshan <agogara132@gmail.com>
2024-04-10 22:20:22 +0300

    Я убрал использование CarController в скрипте врага теперь все работает норм так  же чуь оптимизироввал код

--- Assets/Scripts/Prototip/Enemy.cs
+++ Assets/Scripts/Prototip/Enemy.cs
@@ -1,51 +1,65 @@
 using UnityEngine;
 using NTC.Pool;
-public class Enemy : MonoBehaviour
+using NTC.MonoCache;
+public class Enemy : MonoCache, IPoolable
 {
-    public GameObject player1;
-    public GameObject player2;
     public GameObject[] players;
+    public Transform target;
     public float moveSpeed = 5f;
-    private CharacterController EnemyController;
-    private Vector2 movement;
-    public float Xp = 100;
-    
+    //private Vector2 movement;
+    public float HP = 100;
 
-    void Start(){
-    EnemyController = GetComponent<CharacterController>();
-    players = GameObject.FindGameObjectsWithTag("Player");
-    player1 = players[0];
-    player2 = players[1];
+    public void OnDespawn()
+    {
+        
     }
 
-    void Update(){
-        Vector3 direction1 = player1.transform.position - transform.position;
-        Vector3 direction2 = player2.transform.position - transform.position;
-        //Debug.Log(angle);
-        //Debug.Log(direction);
-        if (Functions.FindNearObject("Player", transform.position)){
-            transform.LookAt(Functions.FindNearObject("Player", transform.position).transform);
-        }
-        if (Xp<=0){
-            //Destroy(transform.gameObject);
-            NightPool.Despawn(gameObject);
-        }
+    public void OnSpawn()
+    {
+        HP = 100;
+    }
+    void Start(){
+    //EnemyController = GetComponent<CharacterController>();
+    players = GameObject.FindGameObjectsWithTag("Player"); 
+    }
+
+    protected override void Run()
+    {
+
+        if(players.Length == 2)
+            if ((transform.position - players[0].transform.position).magnitude <= (transform.position - players[1].transform.position).magnitude)
+                target = players[0].transform;
+            else
+                target = players[1].transform;
+        else if(players[0])
+            target = players[0].transform;
+        
+        transform.LookAt(target);
+        // if (HP<=0){
+        //     Destroy(transform.gameObject);
+        // }
         //if (transform.position.y < 1f || transform.position.y > 1.1f) transform.position = new Vector3(transform.position.x, 1.01f, transform.position.z);// не дает проваливаться под землю, но приколы при контакте
     }
-    private void FixedUpdate() {
-        EnemyController.Move(transform.forward * moveSpeed * Time.deltaTime);
+    protected override void FixedRun() {
+        if ((transform.position - target.position).magnitude > 50)
+            NightPool.Despawn(gameObject);
+        if (target)
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.fixedDeltaTime);
+        
     }
     public void Hit(){
-        Debug.Log(Xp);
-        Xp -=10;
-        
+        Debug.Log(HP);
+        if(HP >= 10){
+            HP -=10;
+        }
+        else NightPool.Despawn(gameObject);
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.tag == "DamageZone")
         {
-            Xp -=10;
+            Hit();
         }
     }
 }
