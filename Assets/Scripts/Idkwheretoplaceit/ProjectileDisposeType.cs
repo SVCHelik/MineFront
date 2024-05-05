382ea5702aae1877802816cf208786c80eebc346
goshan <agogara132@gmail.com>
2024-05-05 18:50:10 +0300

    порядок в папках

--- Assets/Scripts/Idkwheretoplaceit/ProjectileDisposeType.cs
+++
@@ -1,6 +0,0 @@
-public enum ProjectileDisposeType
-    {
-        OnAnyCollision,
-        OnTargetCollision,
-        Manual
-    }
--- Assets/Scripts/Prototip/Player/PlayerObserver.cs
+++ Assets/Scripts/Prototip/Player/PlayerObserver.cs
@@ -3,7 +3,10 @@ public static class PlayerObserver{
     public static Transform Player1Pos;
     public static Transform Player2Pos;
     public static float getPlayersDist(){
-        return Vector3.Distance(Player1Pos.position, Player2Pos.position);
+        if(Is1PlayerAlive && Is2PlayerAlive){
+            return Vector3.Distance(Player1Pos.position, Player2Pos.position);
+        }
+        else return 0;
     }
     public static bool Is1PlayerAlive;
     public static bool Is2PlayerAlive;
